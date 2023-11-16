9150f702dc3b6890971dbb341e4f7bd8bba5f748
DarioM <manghina.dario@gmail.com>
2023-11-16 12:09:08 +0100

    api url global component

--- src/app/global-component.ts
+++ src/app/global-component.ts
@@ -2,12 +2,14 @@ export class GlobalComponent {
 
 
   static dev: boolean = true;
-  static admin: number = 1;
+  static admin: number = 16;
   // 1 loggato come amministratore
   // 2 loggato come idraulico
   // 3 loggato come commerciale
-  //static url_global:string='https://gestionalecero.it/gest_2022';
+  // 16 loggato come Dev
+  //  static url_global: string = 'https://gestionalecero.it/gest_2022';
   static url_global: string = 'http://localhost:4200';
   static key_ruolo: any = localStorage.getItem("id_ruolo");
+  static global_api_url: string = 'https://gestionalecero.it/api/api.php'
 
 }
